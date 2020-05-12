b81f289acddf28d3b9763fcc2ffef74c8e269c06
jazzari <passionjazz@gmail.com>
2020-05-12 10:26:57 +0200

    Starting creating models with TDD

---
+++ app/models/chef.rb
@@ -0,0 +1,3 @@
+class Chef < ApplicationRecord
+    validates :name, presence: true
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_11_163838) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "chefname"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
